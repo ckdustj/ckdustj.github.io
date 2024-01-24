c0a41e952e79eba51c51ac0a598dd1000c26f3ee
ckdustj <152482872+ckdustj@users.noreply.github.com>
2024-01-24 15:47:47 +0900

    Delete docker directory

--- docker/배포용/js/app.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('button');
-const img = document.querySelector('img');
-
-btn.onclick = () => {
-    btn.innerText = "ㅎㅎ";
-    btn.style.backgroundColor = "skyblue";
-    img.style.display = "inline-block";
-}
-

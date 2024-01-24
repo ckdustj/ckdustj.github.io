95e58ad882cdcb8adb040a23b882802f456eecc4
ckdustj <152482872+ckdustj@users.noreply.github.com>
2024-01-24 15:43:31 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-document.querySelector('button').onclick = () => {
+const btn = document.querySelector('button');
+const img = document.querySelector('img');
 
+btn.onclick = () => {
+    btn.innerText = "ㅎㅎ";
+    btn.style.backgroundColor = "skyblue";
+    img.style.display = "inline-block";
 }
+
